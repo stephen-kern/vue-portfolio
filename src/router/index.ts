8f81938924eff46ecd80c595b37807aeadef2ad4
Stephen Kern <stephenkern96@gmail.com>
2025-08-19 18:51:45 -0500

    page structure set up

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,13 @@ import type { RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Projects from "../views/Projects.vue";
-// import Contact from "../views/Contact.vue";
+import Contact from "../views/Contact.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: About },
   { path: "/projects", name: "Projects", component: Projects },
-  //   { path: "/contact", name: Contact, component: Contact },
+  { path: "/contact", name: "Contact", component: Contact },
 ];
 
 const router = createRouter({
