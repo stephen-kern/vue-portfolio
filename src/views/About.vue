36bbb28a34a9965fb2b05d9d5afb38497458736c
Stephen Kern <stephenkern96@gmail.com>
2025-08-19 18:17:19 -0500

    about set up

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const about: string =
+  "I'm a Front End Web Developer trying practicing Vue and TypeScript.";
+</script>
+
+<template>
+    <section>
+        <h1>About Me</h1>
+        <p>{{ about }}</p>
+    </section>
+</template>
