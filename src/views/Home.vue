5e4a965e8a15b91e6728a7d3e76ffc5aeea046e0
Stephen Kern <stephenkern96@gmail.com>
2025-09-21 12:35:05 -0500

    home page paragraph edits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,36 @@
 <script setup lang="ts">
-const name: string = "Stephen Kern"
+const name: string = "Stephen Kern";
 </script>
 
 <template>
-    <section>
-        <h1>Hello, I'm {{ name }} 👋</h1>
-        <p>Welcome to my TypeScript + Vue Portfolio</p>
-    </section>
+  <section>
+    <h1>Hello, I'm {{ name }} 👋</h1>
+    <p>
+      A Front-End Developer passionate about building websites that are clean,
+      user-friendly, and engaging.
+      <br /><br />
+      At Enertech Global, I maintain and improve WordPress and Joomla! sites,
+      create custom PHP features, and implement AJAX-powered filtering to give
+      users smooth, intuitive experiences. I thrive on solving problems,
+      collaborating with teams, and writing code that’s both elegant and
+      functional.
+      <br /><br />
+      My background includes earning a Full-Stack Development Certificate from
+      Washington University in St. Louis, where I gained hands-on experience
+      with React, Node, and SQL.
+      <br /><br />
+      My design approach leans toward clean and organic solutions, creating
+      websites that feel natural, intuitive, and visually appealing while
+      keeping functionality at the forefront.
+      <br /><br />
+      Outside of coding, you’ll find me exploring new music and art, cooking,
+      riding my bike or skateboard, gaming, or planning my next travel
+      adventure.
+      <br /><br />
+      Looking ahead, I’m excited to expand my skills into full-stack and backend
+      development. I enjoy the structure, logic, and problem-solving these areas
+      offer, and I’m eager to take on projects that challenge me to grow as a
+      developer.
+    </p>
+  </section>
 </template>
